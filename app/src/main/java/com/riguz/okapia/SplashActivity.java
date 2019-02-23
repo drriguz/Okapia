542d172a9cd5f1c5e8e5b1a84bec1e7dd96ba728
Riguz Lee <solee.linux@gmail.com>
2019-02-23 09:28:26 +0800

    added init task

--- app/src/main/java/com/riguz/okapia/SplashActivity.java
+++ app/src/main/java/com/riguz/okapia/SplashActivity.java
@@ -1,17 +1,27 @@
 package com.riguz.okapia;
 
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView statusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
 
-        System.out.println("->OnCreate");
+        progressBar = (ProgressBar) findViewById(R.id.progressBarSplash);
+        statusText = (TextView) findViewById(R.id.textViewCurrentStatus);
+
+        new InitializeTask().execute();
     }
 
     @Override
@@ -19,5 +29,40 @@ public class SplashActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
 
         setContentView(R.layout.activity_splash);
+    }
+
+    class InitializeTask extends AsyncTask<Void, String, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                Thread.sleep(1000);
+                publishProgress("Checing data...");
+                Thread.sleep(2000);
+                publishProgress("Done...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            progressBar.setVisibility(View.INVISIBLE);
+            Intent intent = new Intent();
+            intent.setClass(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            statusText.setText(values[0]);
+        }
     }
 }
